4522945ef9e240aa292f4da5825592d4b3453254
anastasiakrbvsk <nastyakarbovskaya1@gmail.com>
2020-09-08 13:12:19 +0300

    feat: add products list and products table

--- app/src/components/Main/Product-Tile.js
+++ app/src/components/Main/Product-Tile.js
@@ -1,11 +1,53 @@
 import React from 'react';
+import PropTypes from "prop-types";
 import './Main.css';
+import ProductList from './ProductList';
 
 class ProductTile extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isAddToCart: false,
+            ProductArr: ProductList
+        };
+    }
+
     render() {
+        const Products = this.state.ProductArr.map((el) => 
+            <tr key={el.id} className='products-item'>
+                <td>{el.star}</td>
+                <td>{el.reliase}</td>
+                <td>{el.manufacturer}</td>
+                <td>{el.model}</td>
+                <td>{el.hash}th/s</td>
+                <td>{el.algorithm}</td>
+                <td>{el.efficiency}j/H/s</td>
+                <td>&#36;{el.profit}/day</td>
+                <td>{el.minPrice}&#36;-{el.maxPrixe}&#36;</td>
+            </tr>
+        );
+
         return (
             <div className='product-tile-wrapper'>
                 <h2 className='product-tile-h2 h2'>On Sale</h2>
+                <table className='products-list'>
+                    <thead>
+                        <tr className='table-head'>
+                            <td></td>
+                            <td>Reliase</td>
+                            <td>Manufacturer</td>
+                            <td>Model</td>
+                            <td>Hash</td>
+                            <td>Algorithm</td>
+                            <td>Efficiency</td>
+                            <td>Profit</td>
+                            <td>Price</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {Products}
+                    </tbody>
+                </table>
             </div>
         )
     }
---
+++ app/src/components/Main/ProductList.js
@@ -0,0 +1,121 @@
+import React from 'react';
+
+const starSvg = 
+    <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M7 9.94758L11.3283 12.9998L9.67167 8.07106L14 5.08671H8.69167L7 -0.000244141L5.30833 5.08671H0L4.32833 8.07106L2.67167 12.9998L7 9.94758Z" fill="#FFFF66"/>
+    </svg>;
+
+const ProductList = [
+    {
+        id: 1,
+        star: starSvg,
+        reliase: 'Mar 2019',
+        manufacturer: 'Bitmain',
+        model: 'S9i',
+        hash: '10.5-14.5',
+        algorithm: 'SHA-256',
+        efficiency: '8.00',
+        profit: 122,
+        minPrice: 137,
+        maxPrixe: 228
+    },
+    {
+        id: 2,
+        star: starSvg,
+        reliase: 'Mar 2019',
+        manufacturer: 'Bitmain',
+        model: 'S9i',
+        hash: '10.5-14.5',
+        algorithm: 'SHA-256',
+        efficiency: '8.00',
+        profit: 122,
+        minPrice: 137,
+        maxPrixe: 228
+    },{
+        id: 3,
+        star: starSvg,
+        reliase: 'Mar 2019',
+        manufacturer: 'Bitmain',
+        model: 'S9i',
+        hash: '10.5-14.5',
+        algorithm: 'SHA-256',
+        efficiency: '8.00',
+        profit: 122,
+        minPrice: 137,
+        maxPrixe: 228
+    },{
+        id: 4,
+        star: starSvg,
+        reliase: 'Mar 2019',
+        manufacturer: 'Bitmain',
+        model: 'S9i',
+        hash: '10.5-14.5',
+        algorithm: 'SHA-256',
+        efficiency: '8.00',
+        profit: 122,
+        minPrice: 137,
+        maxPrixe: 228
+    },{
+        id: 5,
+        star: starSvg,
+        reliase: 'Mar 2019',
+        manufacturer: 'Bitmain',
+        model: 'S9i',
+        hash: '10.5-14.5',
+        algorithm: 'SHA-256',
+        efficiency: '8.00',
+        profit: 122,
+        minPrice: 137,
+        maxPrixe: 228
+    },{
+        id: 6,
+        star: starSvg,
+        reliase: 'Mar 2019',
+        manufacturer: 'Bitmain',
+        model: 'S9i',
+        hash: '10.5-14.5',
+        algorithm: 'SHA-256',
+        efficiency: '8.00',
+        profit: 122,
+        minPrice: 137,
+        maxPrixe: 228
+    },{
+        id: 7,
+        star: starSvg,
+        reliase: 'Mar 2019',
+        manufacturer: 'Bitmain',
+        model: 'S9i',
+        hash: '10.5-14.5',
+        algorithm: 'SHA-256',
+        efficiency: '8.00',
+        profit: 122,
+        minPrice: 137,
+        maxPrixe: 228
+    },{
+        id: 8,
+        star: starSvg,
+        reliase: 'Mar 2019',
+        manufacturer: 'Bitmain',
+        model: 'S9i',
+        hash: '10.5-14.5',
+        algorithm: 'SHA-256',
+        efficiency: '8.00',
+        profit: 122,
+        minPrice: 137,
+        maxPrixe: 228
+    },{
+        id: 9,
+        star: starSvg,
+        reliase: 'Mar 2019',
+        manufacturer: 'Bitmain',
+        model: 'S9i',
+        hash: '10.5-14.5',
+        algorithm: 'SHA-256',
+        efficiency: '8.00',
+        profit: 122,
+        minPrice: 137,
+        maxPrixe: 228
+    },
+];
+
+export default ProductList;
